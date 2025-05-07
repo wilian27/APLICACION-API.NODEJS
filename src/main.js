1ca47d3447b703876bc155d0692a31bda923e2c7
wilian27 <richar.castillo@espoch.edu.ec>
2025-05-07 15:34:56 -0500

    actualizacion

--- src/main.js
+++ src/main.js
@@ -1,27 +1,58 @@
-async function getTrendingMoviesPreview() {
-    const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY);
-    const data = await res.json();
-  
-    const movies = data.results;
-    
-    movies.forEach(movie => {
-      const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList')
-      
-      const movieContainer = document.createElement('div');
-      movieContainer.classList.add('movie-container');
-  
-      const movieImg = document.createElement('img');
-      movieImg.classList.add('movie-img');
-      movieImg.setAttribute('alt', movie.title);
-      movieImg.setAttribute(
-        'src',
-        'https://image.tmdb.org/t/p/w300' + movie.poster_path,
-      );
-  
-      movieContainer.appendChild(movieImg);
-      trendingPreviewMoviesContainer.appendChild(movieContainer);
-    });
-    console.log({data,movies});
-  }
-  
-  getTrendingMoviesPreview();
+const api = axios.create({
+  baseURL: 'https://api.themoviedb.org/3/',
+  headers: {
+    'Content-Type': 'application/json;charset=utf-8',
+  },
+  params: {
+    'api_key': API_KEY,
+  },
+});
+
+async function getTrendingTVPreview() {
+  const { data } = await api('trending/tv/day');
+  const series = data.results;
+
+  const trendingPreviewContainer = document.querySelector('#trendingPreview .trendingPreview-movieList');
+  trendingPreviewContainer.innerHTML = ''; // Limpia contenido previo
+
+  series.forEach(serie => {
+    const seriesContainer = document.createElement('div');
+    seriesContainer.classList.add('movie-container');
+
+    const seriesImg = document.createElement('img');
+    seriesImg.classList.add('movie-img');
+    seriesImg.setAttribute('alt', serie.name);
+    seriesImg.setAttribute(
+      'src',
+      'https://image.tmdb.org/t/p/w300' + serie.poster_path,
+    );
+
+    seriesContainer.appendChild(seriesImg);
+    trendingPreviewContainer.appendChild(seriesContainer);
+  });
+}
+
+async function getCategegoriesPreview() {
+  const { data } = await api('genre/tv/list');
+  const categories = data.genres;
+
+  const previewCategoriesContainer = document.querySelector('#categoriesPreview .categoriesPreview-list');
+  previewCategoriesContainer.innerHTML = '';
+
+  categories.forEach(category => {
+    const categoryContainer = document.createElement('div');
+    categoryContainer.classList.add('category-container');
+
+    const categoryTitle = document.createElement('h3');
+    categoryTitle.classList.add('category-title');
+    categoryTitle.setAttribute('id', 'id' + category.id);
+    categoryTitle.textContent = category.name;
+
+    categoryContainer.appendChild(categoryTitle);
+    previewCategoriesContainer.appendChild(categoryContainer);
+  });
+}
+
+// Llamar las funciones al cargar
+getTrendingTVPreview();
+getCategegoriesPreview();
